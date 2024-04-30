bab521d69dfd7c1c0fdc68bab64d4123d4cc1226
Juan Lima <juangustavo@gmail.com>
2024-04-30 09:08:47 -0300

    arquivos spring initializr

---
+++ src/main/java/br/com/site/literalurachallengeONE/LiterAluraChallengeOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.site.literalurachallengeONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/site/literalurachallengeONE/LiterAluraChallengeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.site.literalurachallengeONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
